33c1cbf248f305de09876efe3feafe2e3909a715
razbensimon <raz.bensimon@env0.com>
2021-12-25 15:32:00 +0200

    git ignore shit
    refactor templates with inheritance
    
    remove unused
    
    add layout template
    
    fix pass max_length
    
    git ignore shit
    
    refactor templates with inheritance
    
    remove unused
    
    add layout template
    
    fix pass max_length

--- app.py
+++ app.py
@@ -260,5 +260,5 @@ def rotate_previous_passwords():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=DEBUG_MODE)
