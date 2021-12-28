5aa75f8b2e16fbe64a0035557e1b851c29bcc5c6
Lior Yakobov <lior.yakobov@sisense.com>
2021-12-28 09:31:18 +0200

    make common passwords list more general

--- passwordValidator.py
+++ passwordValidator.py
@@ -1,4 +1,5 @@
 import configparser
+import requests
 from string import punctuation
 
 config = configparser.RawConfigParser()
@@ -8,42 +9,53 @@ config.read(configFilePath)
 min_length = int(config.get('main', 'min_length'))
 max_length = 16  # hard coded since database column for password is varchar(16)
 passwords_history = int(config.get('main', 'passwords_history'))
-knows_passwords_dictionary = config.get('main', 'dictionary')
+common_passwords_link = config.get('main', 'common_passwords_link')
 
 
-def validate_password(passwd):
-    val = True
+def check_common_passwords(password):
+    resp = requests.get(common_passwords_link)
+    common_passwords_list = resp.text.split('\n')
+
+    for common_password in common_passwords_list:
+        if password == common_password:
+            return False
+
+    return True
+
+
+def validate_password(password):
+    result = True
     info = ""
 
-    if len(passwd) < min_length:
+    if len(password) < min_length:
         info = info + 'Password length should be at least {}\n'.format(min_length)
-        val = False
+        result = False
 
-    if len(passwd) > max_length:
+    if len(password) > max_length:
         info = info + 'Password length should be not be greater than {}\n'.format(max_length)
-        val = False
+        result = False
 
-    if not any(char.isdigit() for char in passwd):
+    if not any(char.isdigit() for char in password):
         info = info + 'Password should have at least one numeric character\n'
-        val = False
+        result = False
 
-    if not any(char.isupper() for char in passwd):
+    if not any(char.isupper() for char in password):
         info = info + 'Password should have at least one uppercase letter\n'
-        val = False
+        result = False
 
-    if not any(char.islower() for char in passwd):
+    if not any(char.islower() for char in password):
         info = info + 'Password should have at least one lowercase letter\n'
-        val = False
+        result = False
 
-    if not any(char in punctuation for char in passwd):
+    if not any(char in punctuation for char in password):
         info = info + 'Password should have at least one special character\n'
-        val = False
+        result = False
 
-    if passwd in knows_passwords_dictionary:
-        info = info + 'Common passwords use is not permitted, please try to use another password\n'
-        val = False
+    if not check_common_passwords(password):
+        info = info + 'Please do not use common passwords, like 12345678...\n'
+        result = False
 
     return {
-        'status': val,
+        'status': result,
         'info': info.split('\n')
     }
