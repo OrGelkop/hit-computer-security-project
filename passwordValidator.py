0b306ff866ae693b3ca681c4cd31ba333835d502
Lior Yakobov <lior.yakobov@sisense.com>
2021-12-28 23:06:34 +0200

    making all password configurations in config.ini

--- passwordValidator.py
+++ passwordValidator.py
@@ -8,6 +8,10 @@ config.read(configFilePath)
 
 min_length = int(config.get('main', 'min_length'))
 max_length = 16  # hard coded since database column for password is varchar(16)
+require_digits = eval(config.get('main', 'require_digits'))
+require_uppercase = eval(config.get('main', 'require_uppercase'))
+require_lowercase = eval(config.get('main', 'require_lowercase'))
+require_symbols = eval(config.get('main', 'require_symbols'))
 passwords_history = int(config.get('main', 'passwords_history'))
 common_passwords_link = config.get('main', 'common_passwords_link')
 
@@ -35,19 +39,20 @@ def validate_password(password):
         info = info + 'Password length should be not be greater than {}\n'.format(max_length)
         result = False
 
-    if not any(char.isdigit() for char in password):
+    if require_digits and not any(char.isdigit() for char in password):
         info = info + 'Password should have at least one numeric character\n'
         result = False
 
-    if not any(char.isupper() for char in password):
+    if require_uppercase and not any(char.isupper() for char in password):
         info = info + 'Password should have at least one uppercase letter\n'
         result = False
 
-    if not any(char.islower() for char in password):
+    if require_lowercase and not any(char.islower() for char in password):
         info = info + 'Password should have at least one lowercase letter\n'
         result = False
 
-    if not any(char in punctuation for char in password):
+    if require_symbols and not any(char in punctuation for char in password):
+        print("checking for symbols")
         info = info + 'Password should have at least one special character\n'
         result = False
 
